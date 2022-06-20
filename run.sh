5430c59bfc057d0b9af30e4c78c69fdd311affe7
Monica Millunzi <82164235+monnieka@users.noreply.github.com>
2022-06-20 17:07:40 +0200

    Delete run.sh

--- run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#SBATCH --partition=students-dev
-#SBATCH --gres=gpu:1
-#SBATCH --output output.txt
-#SBATCH --error error.txt
-
-python main.py new --name hidden0.0 --data-dir /nas/softechict-nas-2/datasets/coco --batch-size 32
