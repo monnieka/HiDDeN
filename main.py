95a261def85d7d710cc56a9bfd9f23085ef4dcbd
Monica Millunzi <mmillunzi@aimagelab-srv-login.ing.unimore.it>
2022-09-06 08:56:10 +0200

    pesi

--- main.py
+++ main.py
@@ -96,7 +96,7 @@ def main():
                                             use_discriminator=True,
                                             use_vgg=False,
                                             discriminator_blocks=3, discriminator_channels=64,
-                                            decoder_loss=1e-3,
+                                            decoder_loss=1,
                                             encoder_loss=0.7,
                                             adversarial_loss=1e-3,
                                             enable_fp16=args.enable_fp16
